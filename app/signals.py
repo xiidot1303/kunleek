7064ccbcf5a30715c8c993e076d778909b734808
xiidot1303 <xii1303@inbox.ru>
2025-08-25 13:04:25 +0500

    use transactio on commit before using celery delay in signals

--- app/signals.py
+++ app/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from app.models import *
 from app.services.newsletter_service import send_order_info_to_group, send_invoice_to_user
 from app.services.order_service import send_order_to_billz
+from django.db import transaction
 
 @receiver(post_save, sender=Order)
 def handle_cash_payment_order(sender, instance: Order, created, **kwargs):
@@ -12,15 +13,21 @@ def handle_cash_payment_order(sender, instance: Order, created, **kwargs):
         handle_order_payment_status_change(sender, instance)
     elif created:
         # send invoice to user with order information
-        send_invoice_to_user.delay(instance.id)
+        transaction.on_commit(
+            lambda: send_invoice_to_user.delay(instance.id)
+        )
 
 @receiver(post_save, sender=Order)
 def handle_order_payment_status_change(sender, instance: Order, **kwargs):
     if instance.payed and not instance.sent_to_group:
         # Perform actions when payed changes to True
-        send_order_info_to_group.delay(instance.id)
         instance.sent_to_group = True
         instance.save(update_fields=["sent_to_group"])
+        transaction.on_commit(
+            lambda: send_order_info_to_group.delay(instance.id)
+        )
 
         # send order to billz
-        send_order_to_billz.delay(instance.id)
+        transaction.on_commit(
+            lambda: send_order_to_billz.delay(instance.id)
+        )
