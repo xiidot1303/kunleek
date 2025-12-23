01aefc9ba2ba26d7bbd1916e49203d21a92b322a
xiidot1303 <xii1303@inbox.ru>
2025-12-23 16:29:41 +0500

    fix sku setting

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -12,7 +12,7 @@ def create_product_from_billz(product_data):
         name = product.get("name").split("/")[0].strip()
         # description = product.get("description", "")
         description = ""
-        sku = product.get("sku")
+        sku = product.get("sku", "")
         main_photo = product.get("main_image_url_full")
         # get quantity
         quantity = 0
@@ -54,7 +54,7 @@ def create_product_from_billz(product_data):
             product_obj.price_without_discount = price
             product_obj.photo = main_photo
             product_obj.photos = photos
-            product_obj.sku = sku,
+            product_obj.sku = sku
             product_obj.quantity = quantity
             products_to_update.append(product_obj)
         else:
