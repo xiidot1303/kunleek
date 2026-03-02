3a1357dc0c24c6e0debf395c2752a0e24933389c
xiidot1303 <xii1303@inbox.ru>
2026-03-02 12:07:42 +0500

    upgrade httprequest for Application builder

--- bot/control/updater.py
+++ bot/control/updater.py
@@ -9,17 +9,29 @@ from telegram.ext import (
     TypeHandler,
     PicklePersistence
 )
+from telegram.request import HTTPXRequest
+import httpx
 from telegram import Update
 from config import BOT_API_TOKEN, WEBHOOK_URL
 from bot.control.handlers import handlers
 from bot.bot.main import error_handler
 from bot import *
 
+transport = httpx.HTTPTransport(retries=3)
+
+request = HTTPXRequest(
+    connect_timeout=30,
+    read_timeout=30,
+    write_timeout=30,
+    connection_pool_size=20,
+    pool_timeout=30,
+    transport=transport
+)
 
 persistence = PicklePersistence(filepath="persistencebot")
 context_types = ContextTypes(context=CustomContext)
 application = Application.builder().token(
-    BOT_API_TOKEN).context_types(context_types).persistence(persistence).build()
+    BOT_API_TOKEN).context_types(context_types).persistence(persistence).request(request).build()
 
 # add handlers
 for handler in handlers[::-1]:
